826177359f63e7102f9da49873e6c0a14fe9a3cb
23060 <2306033612@qq.com>
2017-05-04 19:38:45 +0800

    20170504  吴学坤

---
+++ serverproduct/src/main/java/com/erayic/agr/serverproduct/view/NoScrollWebView.java
@@ -0,0 +1,40 @@
+package com.erayic.agr.serverproduct.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.webkit.WebView;
+
+/**
+ * Created by 23060 on 2017/5/4.
+ */
+
+public class NoScrollWebView extends WebView {
+    public NoScrollWebView(Context context) {
+        super(context);
+    }
+
+    public NoScrollWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NoScrollWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public NoScrollWebView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public NoScrollWebView(Context context, AttributeSet attrs, int defStyleAttr, boolean privateBrowsing) {
+        super(context, attrs, defStyleAttr, privateBrowsing);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        return false;
+    }
+}
---
+++ serverproduct/src/main/java/com/erayic/agr/serverproduct/view/impl/ReportingActivity.java
@@ -0,0 +1,16 @@
+package com.erayic.agr.serverproduct.view.impl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wxk on 2017/5/4.
+ */
+
+public class ReportingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
