5db0e868d910359cb54c73445b5c1886cbd51099
hkceey <hkceey@outlook.com>
2017-05-05 17:08:15 +0800

    2017-05-05 hejian

--- serverproduct/src/main/java/com/erayic/agr/serverproduct/view/impl/ReportingActivity.java
+++ serverproduct/src/main/java/com/erayic/agr/serverproduct/view/impl/ReportingActivity.java
@@ -2,7 +2,10 @@ package com.erayic.agr.serverproduct.view.impl;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -35,8 +38,11 @@ import butterknife.BindView;
 
 @Route(path = "/serverproduct/activity/ReportingActivity", name = "我的服务")
 public class ReportingActivity extends BaseActivity {
+
     @Autowired
     String serviceID;
+    @BindView(R2.id.toolbar)
+    Toolbar toolbar;
     @BindView(R2.id.forecast_reporting_webview)
     NoScrollWebView webView;
 
@@ -45,13 +51,20 @@ public class ReportingActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast_reporting);
     }
+
     @Override
     public void initView() {
+        toolbar.setTitle("天气与农事");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDefaultTextEncodingName("utf-8");
-        webView.addJavascriptInterface(new DataForJsInterface(),"Datas");
+        webView.addJavascriptInterface(new DataForJsInterface(), "Datas");
         webView.loadUrl("file:///android_asset/ReportingTool.html");
-        webView.setWebViewClient(new WebViewClient(){
+        webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
@@ -65,44 +78,55 @@ public class ReportingActivity extends BaseActivity {
             }
         });
     }
+
     @Override
     public void initData() {
 
     }
-    public class  DataForJsInterface{
+
+    public class DataForJsInterface {
         public BaseForecastInfo infos;
         public ReportingInfo reportingInfo;
 
-        public void getDatas(){
-            reportingInfo=new ReportingInfo();
-            reportingInfo.rainDatas=new double[]{2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3};
-            reportingInfo.tmpDatas=new double[]{2.0, 4.9, 4, 3, 5, 3.2, 3.3, 6, 5.2, 5, 4.1, 3.3};
-            reportingInfo.windDatas=new double[]{2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2};
-            reportingInfo.maxRainLabel=300;
-            reportingInfo.minRainLabel=0;
-            reportingInfo.rainYInterval=50;
+        public void getDatas() {
+            reportingInfo = new ReportingInfo();
+            reportingInfo.rainDatas = new double[]{2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3};
+            reportingInfo.tmpDatas = new double[]{2.0, 4.9, 4, 3, 5, 3.2, 3.3, 6, 5.2, 5, 4.1, 3.3};
+            reportingInfo.windDatas = new double[]{2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2};
+            reportingInfo.maxRainLabel = 300;
+            reportingInfo.minRainLabel = 0;
+            reportingInfo.rainYInterval = 50;
         }
-        public void InitDatas(){
-            reportingInfo=new ReportingInfo();
-            reportingInfo.tmpDatas=new double[infos.forecastInfos.size()];
-            reportingInfo.rainDatas=new double[infos.forecastInfos.size()];
-            reportingInfo.windDatas=new double[infos.forecastInfos.size()];
-              for(int i=0;i<infos.forecastInfos.size();i++){
-                  reportingInfo.tmpDatas[i]=infos.forecastInfos.get(i).tmp;
-                  reportingInfo.rainDatas[i]=infos.forecastInfos.get(i).rain;
-                  reportingInfo.windDatas[i]=infos.forecastInfos.get(i).wind;
-              }
+
+        public void InitDatas() {
+            reportingInfo = new ReportingInfo();
+            reportingInfo.tmpDatas = new double[infos.forecastInfos.size()];
+            reportingInfo.rainDatas = new double[infos.forecastInfos.size()];
+            reportingInfo.windDatas = new double[infos.forecastInfos.size()];
+            for (int i = 0; i < infos.forecastInfos.size(); i++) {
+                reportingInfo.tmpDatas[i] = infos.forecastInfos.get(i).tmp;
+                reportingInfo.rainDatas[i] = infos.forecastInfos.get(i).rain;
+                reportingInfo.windDatas[i] = infos.forecastInfos.get(i).wind;
+            }
         }
 
         @JavascriptInterface
-        public String GetData(){
+        public String GetData() {
 //            initData();
             getDatas();
-            String json="";
-            Gson gson=new Gson();
-            json=gson.toJson(reportingInfo);
-            Log.v("wxk",json);
+            String json = "";
+            Gson gson = new Gson();
+            json = gson.toJson(reportingInfo);
+            Log.v("wxk", json);
             return json;
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {//返回
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
