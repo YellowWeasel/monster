67d35d80b0b9ad4b10a6b87834a82a7cb4ecf66c
23060 <2306033612@qq.com>
2017-07-17 10:47:08 +0800

    201707017  吴学坤

---
+++ serverproduct/src/main/java/com/erayic/agr/serverproduct/view/custom/FastWebView.java
@@ -0,0 +1,62 @@
+package com.erayic.agr.serverproduct.view.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.webkit.WebView;
+
+/**
+ * Created by wxk on 2017/7/17.
+ * 防止WebView内存泄漏
+ */
+
+public class FastWebView extends WebView {
+
+    private boolean is_gone = false;
+
+    public FastWebView(Context context) {
+        super(context);
+    }
+
+    public FastWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onWindowVisibilityChanged(int visibility) {
+
+        super.onWindowVisibilityChanged(visibility);
+
+        if (visibility == View.GONE) {
+            try
+            {
+                WebView.class.getMethod("onPause").invoke(this);//停止刷新
+
+            } catch(Exception e) {
+            }
+            this.pauseTimers();
+            this.is_gone = true;
+        } else if(visibility == View.VISIBLE) {
+            try
+            {
+                WebView.class.getMethod("onResume").invoke(this);//继续刷新
+            } catch(Exception e) {
+            }
+            this.resumeTimers();
+            this.is_gone = false;
+        }
+    }
+
+    @Override
+    protected void  onDetachedFromWindow() {
+
+        if(this.is_gone) {
+            try
+            {
+                this.destroy();
+            } catch(Exception e) {
+
+            }
+        }
+    }
+}
